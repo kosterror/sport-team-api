1a5381f6ec4a8ed93145fea3364de379ab9fc856
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-03-17 21:21:29 +0700

    Инициализация проекта

---
+++ src/main/java/ru/kosterror/sportteamapi/SportTeamApiApplication.java
@@ -0,0 +1,21 @@
+package ru.kosterror.sportteamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Основной класс.
+ */
+@SpringBootApplication
+public class SportTeamApiApplication {
+
+    /**
+     * Главный метод.
+     *
+     * @param args параметры.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SportTeamApiApplication.class, args);
+    }
+
+}
