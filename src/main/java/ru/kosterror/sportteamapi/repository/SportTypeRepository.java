c1e9ff2d2c4acb96b4a19f614ad06758cb6f59c3
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-03-18 22:24:14 +0700

    Добавил аннотации @Repository на JPA репозитории.

--- src/main/java/ru/kosterror/sportteamapi/repository/SportTypeRepository.java
+++ src/main/java/ru/kosterror/sportteamapi/repository/SportTypeRepository.java
@@ -1,10 +1,12 @@
 package ru.kosterror.sportteamapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.kosterror.sportteamapi.model.SportType;
 
 /**
  * Интерфейс для взаимодействия с данными в БД, которые представляют собой сущность {@link SportType}.
  */
+@Repository
 public interface SportTypeRepository extends JpaRepository<SportType, Long> {
 }
--- src/main/java/ru/kosterror/sportteamapi/repository/TeamMemberRoleRepository.java
+++ src/main/java/ru/kosterror/sportteamapi/repository/TeamMemberRoleRepository.java
@@ -1,10 +1,12 @@
 package ru.kosterror.sportteamapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.kosterror.sportteamapi.model.TeamMemberRole;
 
 /**
  * Интерфейс для взаимодействия с данными в БД, которые представляют собой сущность {@link TeamMemberRole}.
  */
+@Repository
 public interface TeamMemberRoleRepository extends JpaRepository<TeamMemberRole, Long> {
 }
