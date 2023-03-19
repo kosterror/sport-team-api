8471045358b60e8aa50f93572d58a64958e830da
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-03-19 13:21:44 +0700

    Добавил сваггер и класс для возврата ошибки.

---
+++ src/main/java/ru/kosterror/sportteamapi/dto/sporttype/ApiError.java
@@ -0,0 +1,26 @@
+package ru.kosterror.sportteamapi.dto.sporttype;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Класс, который используется для возврата, на запрос, информации об ошибке.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiError {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-mm-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private HttpStatus httpStatus;
+    private String message;
+    private List<String> errors;
+
+}
